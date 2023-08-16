f00c95541a749d7f9fde19687c57d76c5823d5f9
Aimer Filion <aimerfilion@gmail.com>
2023-08-16 17:06:13 -0400

    Create a new task

---
+++ my-app/src/CreateTask.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const CreateTask = ({ addTask }) => {
+  //   state to keep track of the value in the input
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+
+    addTask(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        placeholder="Create a new todo..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default CreateTask;
