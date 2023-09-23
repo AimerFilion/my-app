8ff9dfd0ef206a2962ba18320c352ab899749368
Aimer Filion <aimerfilion@gmail.com>
2023-09-22 20:27:45 -0400

    Test n1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import Lightswitch from "./Lightswitch";
 import Todo from "./Todo";
 import { useState } from "react";
@@ -6,15 +7,17 @@ const App = () => {
   const [darkMode, setDarkMode] = useState(true);
   const themeClass = darkMode ? "light" : "dark";
   return (
-    <body className={`${darkMode ? "" : "dark"}`}>
-      <div className={`wrapper ${themeClass}`}>
-        <div className="container">
-          <Lightswitch darkMode={darkMode} setDarkMode={setDarkMode} />
-          <Todo darkMode={darkMode} setDarkMode={setDarkMode} />
+    <StrictMode>
+      <body className={`${darkMode ? "" : "dark"}`}>
+        <div className={`wrapper ${themeClass}`}>
+          <div className="container">
+            <Lightswitch darkMode={darkMode} setDarkMode={setDarkMode} />
+            <Todo darkMode={darkMode} setDarkMode={setDarkMode} />
+          </div>
+          {/* <p className="credit">Made by Aimer Filion</p> */}
         </div>
-        {/* <p className="credit">Made by Aimer Filion</p> */}
-      </div>
-    </body>
+      </body>
+    </StrictMode>
   );
 };
 
